2cde940112953b6325fd2e71481926a2101f047e
raul <raulherbert7@gmail.com>
2022-11-08 22:16:46 -0300

    Conection failed with mongoDb

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const Post = require('./models/post');
 const app = express();
+
+mongoose.connect("mongodb+srv://raul:Navalport123@cluster0.slm61ll.mongodb.net/?retryWrites=true&w=majority")
+    .then(() => {
+        console.log('Conected to database!');
+    })
+    .catch((error) => {
+        console.log('Conection failed!');
+        console.log('error', error);
+    })
 
 
 
@@ -23,7 +33,7 @@ app.post("/api/posts", (req, res, next) => {
         title:   req.body.title,
         content: req.body.content
     });
-    console.log(post);
+    post.save();
     res.status(201).json({
         message: "Post added sucessfully!"
     });
