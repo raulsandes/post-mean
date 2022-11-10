aebff9061e82cbc222e36f18c7d98199baa44435
raul <raulherbert7@gmail.com>
2022-11-10 17:20:24 -0300

    Added routing

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Post } from 'src/app/post.model';
 import { PostService } from '../post.service';
 
 @Component({
@@ -7,11 +9,27 @@ import { PostService } from '../post.service';
   templateUrl: './post-create.component.html',
   styleUrls: ['./post-create.component.css']
 })
-export class PostCreateComponent {
+export class PostCreateComponent implements OnInit {
   enteredTitle = "";
   enteredContent = "";
+  private mode = 'create';
+  postId?: string;
+  post?: Post;
 
-  constructor(public postsService: PostService) {}
+  constructor(public postsService: PostService, public route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if(paramMap.has('postId')) {
+        this.mode = 'edit';
+        this.postId = paramMap.get('postId');
+        this.post = this.postsService.getPost(this.postId);
+      } else {
+        this.mode = 'create'
+        this.postId = null;
+      }
+    });
+  }
 
   onAddPost(form: NgForm) {
     if (form.invalid) {
@@ -20,5 +38,6 @@ export class PostCreateComponent {
     this.postsService.addPost(form.value.title, form.value.content);
     form.resetForm();
   }
+
 
 }
